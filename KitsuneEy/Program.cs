1bb0301051787efc6cbba56950a96378b815d988
Milkey Tan <819774989@qq.com>
2018-11-08 10:29:38 +0800

    添加项目文件。

---
+++ KitsuneEy/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+
+namespace KitsuneEy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string domainUrl = "https://cdn.sspai.com/static/js/vue-lazyload.js";
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(domainUrl);
+                request.AllowAutoRedirect = false;
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                Console.WriteLine($"{Convert.ToInt32(response.StatusCode)} {response.StatusCode}");
+                Console.WriteLine("server : " + response.Headers.Get("server"));
+                if (response.Headers.AllKeys.Contains("x-powered-by"))
+                {
+                    Console.WriteLine("x-powered-by : " + response.Headers.Get("x-powered-by"));
+                }
+
+                response.Close();
+            }
+            catch (WebException e)
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                HttpWebResponse response = (HttpWebResponse)e.Response;
+                try
+                {
+                    Console.WriteLine($"{Convert.ToInt32(response.StatusCode)} {response.StatusCode}");
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{exception.Message}");
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{exception.Message} : {domainUrl}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
