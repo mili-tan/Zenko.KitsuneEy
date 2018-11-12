34091172760cac18221621409700924af4823fbf
Milkey Tan <819774989@qq.com>
2018-11-12 08:53:52 +0800

    ResponseEy

--- KitsuneEy/GetResponse.cs
+++ KitsuneEy/GetResponse.cs
@@ -15,6 +15,16 @@ namespace KitsuneEy
             return headers.Get("x-powered-by");
         }
 
+        public static bool GetWebServerContains(WebHeaderCollection headers, string serverStr)
+        {
+            return GetWebServer(headers).Contains(serverStr);
+        }
+
+        public static bool GetXPoweredByContains(WebHeaderCollection headers, string xPoweredByStr)
+        {
+            return GetXPoweredBy(headers).Contains(xPoweredByStr);
+        }
+
         public static bool GetCookieContains(WebHeaderCollection headers,string cookieStr)
         {
             return headers.Get("set-cookie").Contains(cookieStr);
