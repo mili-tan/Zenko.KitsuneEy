582e8eb271291d49c19b92580bc40417a86d8ca2
Milkey Tan <819774989@qq.com>
2018-11-12 08:50:01 +0800

    ResponseEy

---
+++ KitsuneEy/GetResponse.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Net;
+
+namespace KitsuneEy
+{
+    class ResponseEy
+    {
+        public static string GetWebServer(WebHeaderCollection headers)
+        {
+            return headers.Get("server");
+        }
+
+        public static string GetXPoweredBy(WebHeaderCollection headers)
+        {
+            return headers.Get("x-powered-by");
+        }
+
+        public static bool GetCookieContains(WebHeaderCollection headers,string cookieStr)
+        {
+            return headers.Get("set-cookie").Contains(cookieStr);
+        }
+
+        public static bool GetItemContains(WebHeaderCollection headers, string itemStr)
+        {
+            return headers.AllKeys.Contains(itemStr);
+        }
+    }
+}
